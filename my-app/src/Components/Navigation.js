561ae3a62dac5e469efc267d368c2832fa3828b0
DeepikaAmbati <134194779+DeepikaAmbati@users.noreply.github.com>
2024-01-07 17:13:48 +0530

    first commit

---
+++ my-app/src/Components/Navigation.js
@@ -0,0 +1,19 @@
+const Navigation = ()=>
+{
+  return (
+    <div>
+    <nav>
+      <h2>Blood and Medical search platform</h2>
+      <ul>
+        <li href="a">About</li>
+      </ul>
+      <button>Sign in </button>
+      <button>Sign up</button>
+     
+    </nav>
+     <img src="/images/shutterstock_541600759-696x464.jpg" alt="Description" />
+    
+    </div>
+  );
+};
+export default Navigation;
